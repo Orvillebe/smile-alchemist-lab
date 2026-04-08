d27417bf435f0c02f3eb7a1be06aed5da0d64987
Orvillebe <63247728+Orvillebe@users.noreply.github.com>
2026-04-08 16:34:58 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/smile-alchemist-lab">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/process" element={<Process />} />
